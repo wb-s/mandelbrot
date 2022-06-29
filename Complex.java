02e335dcc22497fd6b6a9cdc1768063b8d5bbb07
wb-s <williambenjaminshields@gmail.com>
2022-06-29 03:21:34 -0700

    clean

--- Complex.java
+++ Complex.java
@@ -7,13 +7,6 @@ public class Complex {
 		this.real = real;
 		this.imag = imag;
 	}
-//	public static void main(String[] args) {
-//		Complex myComplex1 = new Complex(1.0, 2.0);
-//		Complex myComplex2 = new Complex(1.0, 2.0);
-//		Complex myComplex3;
-//		myComplex3 = add(myComplex1, myComplex2);
-//		System.out.printf("The sum of two complex numbers is %.1f + %.1fi", myComplex3.real, myComplex3.imag);
-//	}
 	public static Complex ad(Complex num1, Complex num2) {
 		Complex num = new Complex(0.0, 0.0);
 		num.real = num1.real + num2.real;
